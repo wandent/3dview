ffc828cf88f4f3abb5b4d6bf4945b6a4e80da167
Wandenkolk T.Neto <wandent@microsoft.com>
2022-03-21 15:14:14 -0300

    added basic files

---
+++ streamlit-init.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.set_page_config(
+    page_title="PyVista and Plotly Demo", page_icon="⬇", layout="centered"
+)
+
+st.write("matplotlib sample graph")
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 5, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
