0d9174b0608e298d5b81bc3cecd55491a59971ec
Wandenkolk T.Neto <wandent@microsoft.com>
2022-03-21 15:59:23 -0300

    added button to test 3d with Pyvista

--- streamlit-init.py
+++ streamlit-init.py
@@ -38,3 +38,38 @@ fig = go.Figure(data=go.Volume(
     ))
 
 st.plotly_chart(fig)
+
+
+
+# try to run pyvista shapes
+
+# All PyVista Samples here: https://docs.pyvista.org/examples/
+# Draw a uniform grid
+import pyvista as pv
+import numpy as np
+#pv.start_xvfb()
+values = np.linspace(0, 10, 1000).reshape((20, 5, 10))
+values.shape
+
+# Create the spatial reference
+grid = pv.UniformGrid()
+
+
+
+# Set the grid dimensions: shape + 1 because we want to inject our values on
+#   the CELL data
+grid.dimensions = np.array(values.shape) + 1
+
+# Edit the spatial reference
+grid.origin = (100, 33, 55.6)  # The bottom left corner of the data set
+grid.spacing = (1, 5, 2)  # These are the cell sizes along each axis
+
+# Add the data values to the cell data
+grid.cell_data["values"] = values.flatten(order="F")  # Flatten the array!
+
+# Now plot the grid!
+
+if st.button('Display 3D View'):
+    grid.plot(show_edges=True)
+
+
